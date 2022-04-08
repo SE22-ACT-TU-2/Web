9d6844579de4752406956180a154b1aedb5a2822
ChloeNuomiji <chenzhaoying6666@163.com>
2022-04-08 15:12:44 +0800

    add the original web codes

---
+++ js/app.js
@@ -0,0 +1,12 @@
+server = 'https://reedsailing.xyz/api/';
+
+function insert_option(form, select, value, content) {
+    var option = document.createElement('option');
+    option.innerText = content;
+    $(option).attr('value', value);
+    select.append(option);
+}
+
+function time_check(start, end) {
+    return Date.parse(start) <= Date.parse(end);
+}
