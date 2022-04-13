2a8fc2b6364d9d0830df552ea51c49feb3553e30
ChloeNuomiji <chenzhaoying6666@163.com>
2022-04-13 14:40:50 +0800

    add court-add, court-application-add, court-application-confirm, court-batch-edit, court-booking-edit, court-booking-list, court-list, member-batch-edit, member-blacklist, member-confirm.html, and modified index.html, member-list.html in it.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-server = 'https://reedsailing.xyz/api/';
-
+//server = 'https://reedsailing.xyz/api/';
+server = 'http://127.0.0.1:8000/api/';
 function insert_option(form, select, value, content) {
     var option = document.createElement('option');
     option.innerText = content;
