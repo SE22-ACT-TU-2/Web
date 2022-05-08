befac9bb378d4e510557b62b463e2c9783bb2ca8
19373421 <19373421@buaa.edu.cn>
2022-05-08 17:55:44 +0800

    服务器

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //server = 'https://reedsailing.xyz/api/';
 //server = 'http://127.0.0.1:8000/api/';
-server = 'http://114.116.194.3:80/api/';
+server = 'http://114.116.194.3:8000/api/';
 function insert_option(form, select, value, content) {
     var option = document.createElement('option');
     option.innerText = content;
