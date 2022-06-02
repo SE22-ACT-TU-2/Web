6e75e14f5129320aaaf358dc7e4445f5695ba860
huaweiyun <se2022_act_tu_2@126.com>
2022-06-03 00:30:14 +0800

    hmb url

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //server = 'https://reedsailing.xyz/api/';
 //server = 'http://127.0.0.1:8000/api/';
-server = 'http://114.116.194.3:8000/api/';
+// server = 'https://114.116.194.3:8000/api/';
+server = 'https://acttu2.2022martu1.cn/api/';
 function insert_option(form, select, value, content) {
     var option = document.createElement('option');
     option.innerText = content;
